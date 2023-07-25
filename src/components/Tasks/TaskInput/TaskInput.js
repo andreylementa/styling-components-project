867a034b61ad70f257fc28f2c8a28006e3d761b8
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-07-25 15:46:03 +0300

    Styled Components и динамические props

--- src/components/Tasks/TaskInput/TaskInput.js
+++ src/components/Tasks/TaskInput/TaskInput.js
@@ -2,6 +2,34 @@ import React, { useState } from "react";
 
 import Button from "../../UI/Button/Button";
 import "./TaskInput.css";
+import styled from "styled-components";
+
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
+    background: ${(props) =>
+      props.invalid ? "rgb(243, 157, 157)" : "transparent"};
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #c8e1e4;
+    border-color: #00358b;
+  }
+`;
 
 const TaskInput = (props) => {
   const [inputText, setinputText] = useState("");
@@ -24,20 +52,12 @@ const TaskInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isInputValid ? "invalid" : ""}`}>
-        <label
-        //style={{
-        //  color: !isInputValid ? "red" : "black",
-        //}}
-        >
-          Задачи
-        </label>
-        <input
-          //style={{ borderColor: !isInputValid ? "red" : "black" }}
-          type="text"
-          onChange={taskInputChangeHandler}
-        />
-      </div>
+      {/*<div className={`form-control ${!isInputValid ? "invalid" : ""}`}>*/}
+      <FormControl invalid={!isInputValid}>
+        <label>Задачи</label>
+        <input type="text" onChange={taskInputChangeHandler} />
+      </FormControl>
+      {/*</div>*/}
       <Button type="submit">Добавить Задачу</Button>
     </form>
   );
