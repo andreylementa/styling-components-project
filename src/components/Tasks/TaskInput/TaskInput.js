d353593f315832bace6a4271073375258712ba7e
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-07-25 13:54:02 +0300

    Динамическое добавление стилей inline

--- src/components/Tasks/TaskInput/TaskInput.js
+++ src/components/Tasks/TaskInput/TaskInput.js
@@ -6,20 +6,37 @@ import "./TaskInput.css";
 const TaskInput = (props) => {
   const [inputText, setinputText] = useState("");
 
+  const [isInputValid, setIsInputValid] = useState(true);
+
   const taskInputChangeHandler = (event) => {
     setinputText(event.target.value);
   };
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
-    props.onAddTask(inputText);
+    if (inputText.trim().length === 0) {
+      setIsInputValid(false);
+    } else {
+      setIsInputValid(true);
+      props.onAddTask(inputText);
+    }
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Задачи</label>
-        <input type="text" onChange={taskInputChangeHandler} />
+        <label
+          style={{
+            color: !isInputValid ? "red" : "black",
+          }}
+        >
+          Задачи
+        </label>
+        <input
+          style={{ borderColor: !isInputValid ? "red" : "black" }}
+          type="text"
+          onChange={taskInputChangeHandler}
+        />
       </div>
       <Button type="submit">Добавить Задачу</Button>
     </form>
